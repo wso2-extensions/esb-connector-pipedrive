7f986589ce95d4472a17647298350673ca347398
Kesavan Yogarajah <ykesan@gmail.com>
2016-06-29 10:55:48 +0530

    Update PipedriveConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/pipedrive/PipedriveConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/pipedrive/PipedriveConnectorIntegrationTest.java
@@ -55,7 +55,7 @@ public class PipedriveConnectorIntegrationTest extends ConnectorIntegrationTestB
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("pipedrive-connector-1.0.1-SNAPSHOT");
+        init("pipedrive-connector-1.0.1");
         esbRequestHeadersMap = new HashMap<String, String>();
         apiRequestHeadersMap = new HashMap<String, String>();
 
